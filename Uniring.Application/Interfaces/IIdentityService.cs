6c8bb6922eb71915fb1513534558bd7fd7428176
AmirAli Araghi <amirali40223@gmail.com>
2025-09-23 01:03:42 +0330

    refactor: remove unnnecessary usings

---
+++ Uniring.Application/Interfaces/IIdentityService.cs
@@ -0,0 +1,15 @@
+﻿using Uniring.Contracts.Auth;
+
+namespace Uniring.Application.Interfaces
+{
+
+    public interface IIdentityService
+    {
+        Task<(bool Succeeded, IEnumerable<string>? Errors)> RegisterAsync(RegisterRequest request, string? role = null);
+        Task<(bool Success, string? Token, DateTime? ExpiresAt, IEnumerable<string>? Errors)> LoginAsync(LoginRequest request);
+        Task SignOutAsync(); // for Cookie sign-out in UI
+        Task<bool> ConfirmEmailAsync(string userId, string token);
+        // ForgotPassword, ResetPassword, GetUserById ...
+    }
+
+}
