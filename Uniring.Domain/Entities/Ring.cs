52f168cfb0988c8238534af98b66b4f9f9c6be03
AmirAli Araghi <amirali40223@gmail.com>
2025-09-23 21:54:04 +0330

    feat: add new entity Ring.cs

---
+++ Uniring.Domain/Entities/Ring.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uniring.Domain.Entities
+{
+    public class Ring
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public required string uid { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Serial { get; set; }
+
+        [Required]
+        public required int Price { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
