3bef8fcf12f5c83ad560e6f199dcb06e877779d8
AmirAli Araghi <amirali40223@gmail.com>
2025-09-23 23:41:07 +0330

    feat: RingRepository

--- Uniring.Infrastructure/Repositories/RingRepository.cs
+++ Uniring.Infrastructure/Repositories/RingRepository.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Uniring.Application.Interfaces.Repositories;
 
 namespace Uniring.Infrastructure.Repositories
 {
-    internal class RingRepository
+    internal class RingRepository : IRingRepository
     {
     }
 }
