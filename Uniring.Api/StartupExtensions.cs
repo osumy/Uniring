5b207c586753fa230b7aba3055ccf889289d2e3e
AmirAli Araghi <amirali40223@gmail.com>
2025-09-23 22:12:49 +0330

    feat: add StartupExtensions.cs
    + ConfigureServices()
    + ConfigurePipeline()

---
+++ Uniring.Api/StartupExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Uniring.Infrastructure.Entities;
+using Uniring.Infrastructure;
+
+
+namespace Uniring.Api
+{
+    public static class StartupExtensions
+    {
+        public static WebApplication ConfigureServices(
+            this WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            builder.Services.AddControllers();
+
+            //builder.Services.AddApplicationServices();
+            builder.Services.AddInfrastructureServices(builder.Configuration);
+            //builder.Services.AddPersistenceServices(builder.Configuration);
+            //builder.Services.AddIdentityServices(builder.Configuration);
+
+            //builder.Services.AddScoped<ILoggedInUserService, LoggedInUserService>();
+
+            //builder.Services.AddHttpContextAccessor();
+
+            //builder.Services.AddCors(
+            //    options => options.AddPolicy(
+            //        "open",
+            //        policy => policy.WithOrigins([builder.Configuration["ApiUrl"] ?? "https://localhost:7081",
+            //            builder.Configuration["BlazorUrl"] ?? "https://localhost:7080"])
+            //.AllowAnyMethod()
+            //.SetIsOriginAllowed(pol => true)
+            //.AllowAnyHeader()
+            //.AllowCredentials()));
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            //app.MapIdentityApi<ApplicationUser>();
+
+            //app.MapPost("/Logout", async (ClaimsPrincipal user, SignInManager<ApplicationUser> signInManager) =>
+            //{
+            //    await signInManager.SignOutAsync();
+            //    return TypedResults.Ok();
+            //});
+
+            //app.UseCors("open");
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            //app.UseCustomExceptionHandler();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
