aedf430b014b01bb9f41b44ce4a2b81a40ce5d37
AmirAli Araghi <amirali40223@gmail.com>
2025-09-24 10:51:40 +0330

    feat: add new file Account Controller

---
+++ Uniring.App/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Uniring.App.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
