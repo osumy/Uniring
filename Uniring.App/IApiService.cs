aa9c7ec149bc7572215c749e653f3dbf6c1f95ec
AmirAli Araghi <amirali40223@gmail.com>
2025-09-24 16:41:49 +0330

    feat: add interface IApiService
    + GetRingByUidAsync
    + GetRingBySerialAsync

---
+++ Uniring.App/IApiService.cs
@@ -0,0 +1,11 @@
+﻿using Uniring.Contracts.Ring;
+
+namespace Uniring.App
+{
+    public interface IApiService
+    {
+        Task<RingResponse?> GetRingByUidAsync(string uid);
+        Task<RingResponse?> GetRingBySerialAsync(string serial);
+
+    }
+}
