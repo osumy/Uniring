14cf8b936c7bd63e7f132a27d8338dc9b33ab767
AmirAli Araghi <amirali40223@gmail.com>
2025-09-24 16:40:34 +0330

    feat: add Farsi and Arabic Login and Signup view

--- Uniring.App/Controllers/AccountController.cs
+++ Uniring.App/Controllers/AccountController.cs
@@ -4,14 +4,27 @@ namespace Uniring.App.Controllers
 {
     public class AccountController : Controller
     {
-       
+
+        [Route("login")]
         public IActionResult Login()
         {
             return View();
         }
 
+        [Route("signup")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
 
-        public IActionResult Register()
+        [Route("login-ar")]
+        public IActionResult LoginAr()
+        {
+            return View();
+        }
+
+        [Route("signup-ar")]
+        public IActionResult SignupAr()
         {
             return View();
         }
