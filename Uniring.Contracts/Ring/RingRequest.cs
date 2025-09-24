ed851773149d6136df39aa04b06fb3b3d94298c6
AmirAli Araghi <amirali40223@gmail.com>
2025-09-24 16:40:58 +0330

    feat: add Dto RingRequest

---
+++ Uniring.Contracts/Ring/RingRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Uniring.Contracts.Ring
+{
+    public class RingRequest
+    {
+        public required string Uid { get; set; }
+
+        public required string Serial { get; set; }
+
+    }
+}
