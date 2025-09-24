eca0f10d0ab3a4c49fec2b60bbd4b12b8287bcbe
AmirAli Araghi <amirali40223@gmail.com>
2025-09-24 23:46:26 +0330

    feat: RegisterRequest validations

--- Uniring.Contracts/Auth/RegisterRequest.cs
+++ Uniring.Contracts/Auth/RegisterRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,21 @@ namespace Uniring.Contracts.Auth
     /// - PhoneNumber is required and used for login
     /// - Password is required
     /// </summary>
-    public record RegisterRequest(
-        string DisplayName,
-        string PhoneNumber,
-        string Password
-        );
+    public class RegisterRequest
+    {
+        [Required(ErrorMessage = "Name Can't be blank")]
+        public required string DisplayName { get; set; }
+
+        [Required(ErrorMessage = "Phone Number Can't be blank")]
+        // TODO: Regex 
+        public required string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Password Can't be blank")]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password Can't be blank")]
+        [DataType(DataType.Password)]
+        public required string ConfirmPassword { get; set; }
+    }
 }
