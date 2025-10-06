ce24e75dd33acedfd8706f6428f692b24baf80fd
AmirAli Araghi <amirali40223@gmail.com>
2025-10-06 08:45:56 +0330

    feat: add AddApplicationServices()

--- Uniring.Application/ApplicationServiceRegistration.cs
+++ Uniring.Application/ApplicationServiceRegistration.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Uniring.Application.Interfaces;
+using Uniring.Application.Services;
 
 namespace Uniring.Application
 {
-    public class ApplicationServiceRegistration
+    public static class ApplicationServiceRegistration
     {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRingService, RingService>();
 
+            return services;
+        }
     }
 }
