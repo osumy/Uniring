36eb5df8b766dca4e51b595c6b306ca381c0886c
AmirAli Araghi <amirali40223@gmail.com>
2025-10-06 13:48:06 +0330

    refactore: move IdentityService to Application project

--- Uniring.Domain/Entities/IdentityEntities/ApplicationRole.cs
+++ Uniring.Domain/Entities/IdentityEntities/ApplicationRole.cs
@@ -1,13 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Uniring.Domain.Entities.IdentityEntities
 {
-    public class ApplicationRole : IdentityRole<Guid>
-    {
-    }
+    public class ApplicationRole : IdentityRole<Guid> {}
 }
