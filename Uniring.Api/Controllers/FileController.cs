f33cc25e7a06f74c29e32d0aada42662434dc9c7
AmirAli Araghi <amirali40223@gmail.com>
2025-10-10 17:51:28 +0330

    feat: add new controller: FileController
    for downloading and uploading images and videos

---
+++ Uniring.Api/Controllers/FileController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Uniring.Api.Controllers
+{
+    public class FileController : ApiControllerBase
+    {
+        [HttpGet("download-video")]
+        public async Task<IActionResult> GetVideoById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("download-image")]
+        public async Task<IActionResult> GetImageById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("upload-video")]
+        public async Task<IActionResult> UploadVideo(IFormFile file)
+        {
+            return Ok();
+        }
+
+        [HttpPost("upload-image")]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            return Ok();
+        }
+    }
+}
