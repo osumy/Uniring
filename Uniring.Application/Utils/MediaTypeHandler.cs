c1c886656a632a935f44c36a2239068144c224e3
AmirAli Araghi <amirali40223@gmail.com>
2025-10-20 14:36:05 +0330

    feat: add MediaTypeHandler
    to check type of file

--- Uniring.Application/Utils/MediaTypeHandler.cs
+++ Uniring.Application/Utils/MediaTypeHandler.cs
@@ -1,6 +1,24 @@
 ﻿namespace Uniring.Application.Utils
 {
-    public class MediaTypeHandler
+    public static class MediaTypeHandler
     {
+        private static readonly HashSet<string> AllowedImageExtensions = new(StringComparer.OrdinalIgnoreCase) { ".jpg", ".jpeg", ".png", ".gif", ".webp" };
+
+        private static readonly HashSet<string> AllowedVideoExtensions = new(StringComparer.OrdinalIgnoreCase) { ".mp4", ".webm", ".mov", ".mkv" };
+
+        public static bool isValidVideo(string ext)
+        {
+            return AllowedVideoExtensions.Contains(ext);
+        }
+
+        public static bool isValidImage(string ext)
+        {
+            return AllowedImageExtensions.Contains(ext);
+        }
+
+        public static bool isValidMedia(string ext)
+        {
+            return isValidImage(ext) || isValidVideo(ext);
+        }
     }
 }
