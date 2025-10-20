25a9d4ea83805082a6c6126f7e2fccb85e054f35
AmirAli Araghi <amirali40223@gmail.com>
2025-10-20 14:33:58 +0330

    feat: add RegisterAsync to IApiService

--- Uniring.App/IApiService.cs
+++ Uniring.App/IApiService.cs
@@ -1,4 +1,6 @@
-﻿using Uniring.Contracts.Ring;
+﻿using Uniring.Contracts.ApiResult;
+using Uniring.Contracts.Auth;
+using Uniring.Contracts.Ring;
 
 namespace Uniring.App
 {
@@ -6,6 +8,7 @@ namespace Uniring.App
     {
         Task<RingResponse?> GetRingByUidAsync(string uid);
         Task<RingResponse?> GetRingBySerialAsync(string serial);
+        Task<RegisterResponse> RegisterAsync(RegisterRequest requestModel);
 
     }
 }
