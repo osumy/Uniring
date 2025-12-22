3a1eb02b95d29373b9d3dfdc333899a89930017b
AmirAli Araghi <amirali40223@gmail.com>
2025-12-22 13:37:17 +0330

    refactor: LoginReguest record to class

--- Uniring.Contracts/Auth/LoginRequest.cs
+++ Uniring.Contracts/Auth/LoginRequest.cs
@@ -1,10 +1,18 @@
-﻿namespace Uniring.Contracts.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Uniring.Contracts.Auth
 {
     /// <summary>
     /// Login request: PhoneNumber + Password only.
     /// </summary>
-    public record LoginRequest(
-        string PhoneNumber,
-        string Password
-        );
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "Phone Number Can't be blank")]
+        // TODO: Regex 
+        public required string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Password Can't be blank")]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+    }
 }
